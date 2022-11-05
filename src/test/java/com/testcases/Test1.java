e8d55f195dc7309ff0d5826f842cffac639a0a2f
Suresh Pelluru <Suresh Pelluru@DESKTOP-B4ND95H>
2022-11-05 09:14:33 +0530

    First commit

---
+++ src/test/java/com/testcases/Test1.java
@@ -0,0 +1,20 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test3");
+	}
+
+}
